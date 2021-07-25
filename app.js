186ed9b4ec56704ef59b32b6e390f635f4c78e39
Duoc <duthanhduoc@gmail.com>
2021-07-25 07:54:28 +0700

    update comment

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
+// Thứ tự nhập
 let turn = 1
+// Mảng chứa các số đã nhập
 let numbers = []
+// Function tạo element
+// element này sẽ được chèn vào trong <div class="magic-numbers"></div>
 const createMagicNumber = (className, role, value) => {
   const node = document.createElement('div')
   node.className = className
@@ -12,12 +16,19 @@ const createMagicNumber = (className, role, value) => {
 const magicNumbersNode = document.querySelector('.magic-numbers')
 const resultNode = document.querySelector('.result')
 const formNode = document.querySelector('form')
-formNode.addEventListener('submit', event => {
+
+// Lắng nghe sự kiện submit số
+formNode.addEventListener('submit', (event) => {
   event.preventDefault()
   const value = document.querySelector('form input').value
+  // Tạo element của bạn
   const yourNode = createMagicNumber('number number-you', 'Bạn', value)
+  // Reset ô input sau khi submit
   document.querySelector('form input').value = ''
+  // Nếu là lần đầu tiên thì chỉ cần chèn 1 node là node của bạn
   if (turn === 1) {
+    // Tính nhanh kết quả
+    // công thức phía dưới tương đương Number(value) + 19998
     result = Number('2' + value) - 2
     resultNode.innerHTML = `
     <div class="result-overlay"></div>
@@ -28,19 +39,29 @@ formNode.addEventListener('submit', event => {
     numbers.push(Number(value))
     turn++
   } else {
-    const myNode = createMagicNumber('number number-me', 'Mình', 9999 - Number(value))
+    // Nếu là lần thứ 2 trở đi thì chèn 2 node vào
+    // là node của bạn và node của mình
+    const myNode = createMagicNumber(
+      'number number-me',
+      'Mình',
+      9999 - Number(value)
+    )
     magicNumbersNode.appendChild(yourNode)
     magicNumbersNode.appendChild(myNode)
     numbers.push(Number(value), 9999 - Number(value))
     turn += 2
   }
+  // Nếu là lượt cuối thì sẽ chèn thêm element tính tổng
   if (turn === 6) {
     const sumValue = numbers.reduce((current, result) => result + current)
     const sumNode = createMagicNumber('number number-sum', 'Tổng', sumValue)
     magicNumbersNode.appendChild(sumNode)
+    // Ẩn cái ô input đi, không cho nhập nữa
     formNode.style.display = 'none'
   }
 })
+
+// Lắng nghe sự kiện nhấn button reset
 document.querySelector('.btn-reset').addEventListener('click', () => {
   magicNumbersNode.innerHTML = ''
   resultNode.innerHTML = ''
@@ -49,6 +70,9 @@ document.querySelector('.btn-reset').addEventListener('click', () => {
   numbers = []
   formNode.style = null
 })
+
+// Lắng nghe sự kiện nhấn vào khu vực result.
+// Chúng ta sẽ thực hiện làm mờ hoặc xóa làm mờ.
 resultNode.addEventListener('click', () => {
   document.querySelector('.result-overlay').classList.toggle('hide')
 })
